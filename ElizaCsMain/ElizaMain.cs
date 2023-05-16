ff255911686a5c4cc2bcf1f3938b7b35f25e72db
Mike Ferrell <mikeferrell.neo@gmail.com>
2023-05-16 10:53:00 -0400

    * Class ElizaMain now offers the initial console output (including the app title banner) as readonly string property CurrentOutput . * Also added readonly string property LatestInput, which currently just returns null. * Testing class Test_ElizaMain_New (file Test_ElizaMain) now has testing methods Test_CurrentOutput and Test_LatestInput as well as a string variable for comparing to CurrentOutput.

--- ElizaCsMain/ElizaMain.cs
+++ ElizaCsMain/ElizaMain.cs
@@ -2,6 +2,23 @@
 {
     public class ElizaMain
     {
+        private const string _Eol = "\n";
 
+        public string CurrentOutput
+        {
+            get
+            {
+                return
+                        new string(' ', 26) + "ELIZA" + _Eol +
+                        new string(' ', 20) + "CREATIVE COMPUTING" + _Eol +
+                        new string(' ', 18) + "MORRISTOWN, NEW JERSEY" + _Eol + _Eol + _Eol +
+                        "HI!  I'M ELIZA.  WHAT'S YOUR PROBLEM?";
+            }
+        }
+
+        public string LatestInput
+        {
+            get { return null; }
+        }
     }
 }
--- Test_ElizaCsMain/Test_ElizaMain.cs
+++ Test_ElizaCsMain/Test_ElizaMain.cs
@@ -1,10 +1,19 @@
+//See Eliza in David Ahl's _More_Basic_Computer_Games_
+//E.g.: https://www.atariarchives.org/morebasicgames/showpage.php?page=56
 using ElizaCsMain;
+using System.Security.Cryptography.X509Certificates;
 
 namespace Test_ElizaCsMain
 {
     [TestClass]
     public class Test_ElizaMain_New
     {
+        private const string _Eol = "\n";
+        private string _InitialOutput = 
+                new string(' ', 26) + "ELIZA" + _Eol +
+                new string(' ', 20) + "CREATIVE COMPUTING" + _Eol +
+                new string(' ', 18) + "MORRISTOWN, NEW JERSEY" + _Eol + _Eol + _Eol +
+                "HI!  I'M ELIZA.  WHAT'S YOUR PROBLEM?";
         private ElizaMain _Main;
 
         [TestInitialize]
@@ -17,6 +26,18 @@ namespace Test_ElizaCsMain
         public void Test_Nullness()
         {
             Assert.IsNotNull(_Main);
+        }
+
+        [TestMethod]
+        public void Test_CurrentOutput()
+        {
+            Assert.AreEqual(_InitialOutput, _Main.CurrentOutput);
+        }
+
+        [TestMethod]
+        public void Test_LatestInput()
+        {
+            Assert.IsNull(_Main.LatestInput);
         }
     }
 }
